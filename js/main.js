a5d48fd839c91bca7f799a5df7bbedf572abfc77
valikwork <50335036+valikwork@users.noreply.github.com>
2019-05-31 01:08:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,21 +14,51 @@ $('.services-slick').slick({
     nextArrow: $('.main-slider__next')
 });
 
+var tabWrap = document.querySelector('.solution__tabs-wrap');
 
-document.querySelector('.solution__tabs-wrap').addEventListener('click', function(event) {
-    var el = event.target;
-    if(el.classList.contains('solution__btn')) {
-        var tar = el.dataset.target;
-        tabs.forEach(function(tab) {
-            tab.classList.add('hide');
-            if (tab.id === tar) {
-                tab.classList.remove('hide');
-            };
-        });
+if(tabWrap) {
+    tabWrap.addEventListener('click', function(event) {
+        var el = event.target;
         
-        tabBtns.forEach(function(btn) {
-            btn.classList.remove('active');
-        })
-        el.classList.add('active');
-    };
+        if(el.classList.contains('solution__btn')) {
+            var tar = el.dataset.target;
+            tabs.forEach(function(tab) {
+                tab.classList.add('hide');
+                if (tab.id === tar) {
+                    tab.classList.remove('hide');
+                };
+            });
+            
+            tabBtns.forEach(function(btn) {
+                btn.classList.remove('active');
+            })
+            el.classList.add('active');
+        };
+    });
+};
+
+var accordionBodies = $('.expert-accordion__item-body').hide();
+var accordionHeads = $('.expert-accordion__item-head');
+var btns = $('.acc-btn');
+
+btns.html('<i class="fas fa-plus"></i>');
+
+$('.acc-btn').on('click', function(e) {
+    // var el = $(e.target);
+
+    var thisBody = $(this).parent().next();
+    var thisHead = $(this).parent();
+    
+    btns.html('<i class="fas fa-plus"></i>');
+    accordionBodies.slideUp();
+    accordionHeads.removeClass('open');
+    
+    thisBody.slideDown();
+    thisHead.addClass('open');
+    $(this).html('<i class="fas fa-minus"></i>');
+
 });
+
+
+
+
