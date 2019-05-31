f6dd519414feede2b745a6b0e3685d7b38823521
valikwork <50335036+valikwork@users.noreply.github.com>
2019-06-01 00:12:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -43,22 +43,49 @@ var btns = $('.acc-btn');
 
 btns.html('<i class="fas fa-plus"></i>');
 
-$('.acc-btn').on('click', function(e) {
-    // var el = $(e.target);
+$('.acc-btn').on('click', function() {
 
     var thisBody = $(this).parent().next();
     var thisHead = $(this).parent();
     
-    btns.html('<i class="fas fa-plus"></i>');
-    accordionBodies.slideUp();
-    accordionHeads.removeClass('open');
-    
-    thisBody.slideDown();
-    thisHead.addClass('open');
-    $(this).html('<i class="fas fa-minus"></i>');
-
+    if(thisHead.hasClass('open')) {
+        thisHead.removeClass('open');
+        btns.html('<i class="fas fa-plus"></i>');
+        thisBody.slideUp();
+    } else {
+        btns.html('<i class="fas fa-plus"></i>');
+        accordionBodies.slideUp();
+        accordionHeads.removeClass('open');
+        
+        thisBody.slideDown();
+        thisHead.addClass('open');
+        $(this).html('<i class="fas fa-minus"></i>');
+    };
 });
 
 
+var expertTabs = document.querySelectorAll('.experts-tabs__card');
+var expertsTabBtns = document.querySelectorAll('.experts-tabs__btn');
+var tabExpertsWrap = document.querySelector('.experts-tabs__wrap');
 
+if(tabExpertsWrap) {
+    tabExpertsWrap.addEventListener('click', function(event) {
+        var el = event.target;
+    
+        if(el.classList.contains('experts-tabs__btn')) {
+            var tar = el.dataset.target;
+            expertTabs.forEach(function(tab) {
+                tab.classList.add('hide');
+                if (tab.id === tar) {
+                    tab.classList.remove('hide');
+                };
+            });
+            
+            expertsTabBtns.forEach(function(btn) {
+                btn.classList.remove('active');
+            });
+            el.classList.add('active');
+        };
+    });
+};
 
